fa43dcbf36c164fa26f363349de40e4cb3fddb4f
DottoLuan <luandotto.ld@gmail.com>
2024-05-23 12:21:39 -0300

    start configuration cypress

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = {
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config)
     },
-    baseUrl: 'https://hackernews-seven.vercel.app', 
+    baseUrl: 'https://hawk.ambevdevs.com.br', 
     fixturesFolder: 'cypress/fixtures'
   }
 }
